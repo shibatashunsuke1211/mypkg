9b47b9b0ad9129538a03cf3ee313f68d139f1bd3
ShunsukeShibata <s23C1068BM@s.chibakoudai.jp>
2025-01-11 20:24:03 +0900

    rename

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,22 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2025 Shunsuke Shibata
+# SPDX-FileCopyrightText: 2025 Shunsuke Shibata
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
 
+# ROS2 ワークスペースに移動
 cd $dir/ros2_ws
+
+# ROS2 パッケージをビルド
 colcon build
 source $dir/.bashrc
-timeout 75 ros2 launch mypkg cpu_usage.launch | tee - /tmp/mypkg.log
 
-cat /tmp/mypkg.log | 
-grep 'CPU Usage:'
+# ノードを直接実行して、CPU使用率をパブリッシュする
+timeout 75 ros2 run mypkg cpu_usage_listener | tee /tmp/mypkg.log
+
+# ログの内容を確認
+cat /tmp/mypkg.log | grep 'Received CPU Usage:'
+
 
 
